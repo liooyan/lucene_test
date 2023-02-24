0fd8c280fe66ad40b7ba22c969abc67b7870f20f
李延 <liyan@wondersoft.cn>
2023-02-24 18:47:15 +0800

    Lucene84PostingsWriter

---
+++ src/main/java/cn/lioyan/lucene/codecs/lucene84/Lucene84PostingsWriter.java
@@ -0,0 +1,15 @@
+package cn.lioyan.lucene.codecs.lucene84;
+
+/**
+ * {@link org.apache.lucene.codecs.lucene84.Lucene84PostingsWriter}
+ *<pre>
+ *     doc 文档生成
+ *
+ *
+ *</pre>
+ * @author com.lioyan
+ * @date 2023/2/24  18:46
+ */
+public class Lucene84PostingsWriter
+{
+}
