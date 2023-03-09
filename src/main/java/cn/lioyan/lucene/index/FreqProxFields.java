04f50de248a506b470344748c3def4465756bfc9
李延 <liyan@wondersoft.cn>
2023-03-09 16:00:46 +0800

    完善 ByteBlockPool

---
+++ src/main/java/cn/lioyan/lucene/index/FreqProxFields.java
@@ -0,0 +1,25 @@
+package cn.lioyan.lucene.index;
+
+/**
+ * {@link org.apache.lucene.index.FreqProxFields}
+ *<pre>
+ *     分词字段集合，内部包含  Map<String,FreqProxTermsWriterPerField> 。保存分词结果
+ *
+ *
+ *</pre>
+ * @author com.lioyan
+ * @date 2023/3/9  11:54
+ */
+public class FreqProxFields
+{
+
+    /**
+     *  {@link org.apache.lucene.index.FreqProxFields.FreqProxTermsEnum}
+     *
+     *
+     *
+     */
+    public static class FreqProxTermsEnum{
+
+    }
+}
