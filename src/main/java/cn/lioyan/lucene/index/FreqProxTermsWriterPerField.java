a42e362b8a44162d83887a5728a97a4ae4c78b5a
李延 <liyan@wondersoft.cn>
2023-03-09 09:18:30 +0800

    完成 FreqProxTermsWriterPerField

--- src/main/java/cn/lioyan/lucene/index/FreqProxTermsWriterPerField.java
+++ src/main/java/cn/lioyan/lucene/index/FreqProxTermsWriterPerField.java
@@ -2,7 +2,13 @@ package cn.lioyan.lucene.index;
 
 /**
  * {@link org.apache.lucene.index.FreqProxTermsWriterPerField}
+ *<pre>
+ *     继承自{@link TermsHashPerField}
+ *     bytePool 被分为两个 流
+ *     第一个 k-v 方式。 k：docId（差值） ，出现次数
+ *     第二个 为 position 位置， 前面出现次数 对应一个id就有几个
  *
+ *</pre>
  *
  *
  * https://juejin.cn/post/7087218915893313544
