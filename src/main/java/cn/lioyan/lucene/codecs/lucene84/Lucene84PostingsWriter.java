bcf0e85feda5ddf0341c903d054fee527d5e1af1
李延 <liyan@wondersoft.cn>
2023-03-14 14:32:50 +0800

    put

--- src/main/java/cn/lioyan/lucene/codecs/lucene84/Lucene84PostingsWriter.java
+++ src/main/java/cn/lioyan/lucene/codecs/lucene84/Lucene84PostingsWriter.java
@@ -8,6 +8,7 @@ package cn.lioyan.lucene.codecs.lucene84;
  *
  * startTerm 一个term开始时调用
  * startDoc  记录这个term出现的文档与频率
+ * addPosition 添加 position 相关数据
  * finishDoc 完成当前文档的term记录
  * finishTerm 完成当前term时调用
  *
